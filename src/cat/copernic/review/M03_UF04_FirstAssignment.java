840ed38d1663be7984ea9f8c1af88dc8489f8674
mireialozano <mireia.ls@terrassa.salesians.cat>
2020-10-15 20:24:42 +0200

    Barcos

--- src/cat/copernic/review/M03_UF04_FirstAssignment.java
+++ src/cat/copernic/review/M03_UF04_FirstAssignment.java
@@ -13,13 +13,18 @@ public class M03_UF04_FirstAssignment {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Prueba to código aquí
+        Barco barco1 = new Yate(true, 4, 6, new Motor(), 20.0, 12.5, 23, 71);
+        Barco barco2 = new Portaviones(35, new Motor(), 250, 75, 46, 60000, 7);
+        
+        System.out.println(barco1 instanceof Portaviones);
+       /* // Prueba to código aquí
         CuentaCorriente cc1 = new CuentaCorriente("Pedro", 1_000.0);
         System.out.println(cc1);
         cc1.ingresa(123.09);
         System.out.println(cc1);
         cc1.abona(98.87);
         System.out.println(cc1);
+*/
     }
     
 }
