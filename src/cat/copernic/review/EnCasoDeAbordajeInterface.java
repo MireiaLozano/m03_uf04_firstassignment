3ad960402bb3df323b11dc8351d7fb82624663c4
Mireia Lozano Sanchez <mireia.ls@terrassa.salesians.cat>
2020-10-17 18:31:22 +0200

    Clase en caso de abordaje

---
+++ src/cat/copernic/review/EnCasoDeAbordajeInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cat.copernic.review;
+
+
+
+
+/**
+ *
+ * @author User
+ */
+public interface EnCasoDeAbordajeInterface {
+    //Cuando aborden el barco
+    void comportamiento();
+}
