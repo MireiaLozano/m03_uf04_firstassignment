86352b62a1e1c0132bf359e5772d23bdbe0d4d41
Mireia Lozano Sanchez <mireia.ls@terrassa.salesians.cat>
2020-10-17 18:21:54 +0200

    Clase barco de mercancias deberes

---
+++ src/cat/copernic/review/BarcoDeMercancias.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cat.copernic.review;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public abstract class BarcoDeMercancias extends Barco {
+        private int tiposMercancias;
+
+    public BarcoDeMercancias(int tiposMercancias, Motor motor, double eslora, double manga, int nudos, int peso) {
+        super(motor, eslora, manga, nudos, peso);
+        this.tiposMercancias = tiposMercancias;
+    }
+   
+    @Override
+    public void arranca(){
+        System.out.println("Arrancando barco de mercancias");
+    }
+    
+    @Override
+     public void atraca(){
+        System.out.println("Atracando barco de mercancias");
+    }
+     
+     public void abandonenElBarco() {
+        System.out.println("Tiren la mercancía al mar o saquen lo que puedan y salgan del barco");
+    }
+     
+     public void comportamiento(){
+         System.out.println("Salven toda la mercancía posible");
+     }
+}
