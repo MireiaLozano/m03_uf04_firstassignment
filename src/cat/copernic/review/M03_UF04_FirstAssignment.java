71ae90ad9a906e5dfc12493888fb2ab91783a82d
Mireia Lozano Sanchez <mireia.ls@terrassa.salesians.cat>
2020-10-17 19:20:09 +0200

    Añadidos los 3 buques de mercancías

--- src/cat/copernic/review/M03_UF04_FirstAssignment.java
+++ src/cat/copernic/review/M03_UF04_FirstAssignment.java
@@ -19,16 +19,20 @@ public class M03_UF04_FirstAssignment {
         Barco barco2 = new Portaviones(35, new Motor(456), 250, 75, 46, 60000, 7);
         barco2.arranca();
         
-        Barco buque1 = new PortaContenedores(450, "Contenedores", new Motor(546), 400, 59, 25, 14);
+        Barco buque1 = new PortaContenedores(450, "Contenedores", (new Motor(546)), 400, 59, 25, 14);
+        System.out.println(buque1);
         buque1.arranca();
         
-        Barco buque2 = new BuquePetrolero(2000000, "Derivados del petróleo", new Motor(645), 458, 48, 12, 158);
+        Barco buque2 = new BuquePetrolero(2000000, "Derivados del petróleo", (new Motor(645)), 458, 48, 12, 158);
+        System.out.println(buque2);
         buque2.arranca();
         
-        Barco buque3 = new BuqueQuimico(250000, 6, "Fenol, Hidróxido de sodio, Metanol, Ácido sulfúrico, Ácido nítrico y Tolueno" , "Productor químicos", new Motor(780), 150, 92.86, 25, 14);
+        Barco buque3 = new BuqueQuimico(250000, 6, "Fenol, Hidróxido de sodio, Metanol, Ácido sulfúrico, Ácido nítrico y Tolueno" , "Productor químicos", (new Motor(780)), 150, 92.86, 25, 14);
+        System.out.println(buque3);
         buque3.arranca();
+       
         
-        System.out.println(barco1 instanceof Portaviones);
+       //System.out.println(barco1 instanceof Portaviones);
        /* // Prueba to código aquí
         CuentaCorriente cc1 = new CuentaCorriente("Pedro", 1_000.0);
         System.out.println(cc1);
