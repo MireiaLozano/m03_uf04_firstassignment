5c7da0e634f0281fd15f6f496e0d49e924cc48dc
mireialozano <mireia.ls@terrassa.salesians.cat>
2020-10-17 17:38:42 +0200

    Barcos

--- src/cat/copernic/review/M03_UF04_FirstAssignment.java
+++ src/cat/copernic/review/M03_UF04_FirstAssignment.java
@@ -13,8 +13,8 @@ public class M03_UF04_FirstAssignment {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Barco barco1 = new Yate(true, 4, 6, new Motor(), 20.0, 12.5, 23, 71);
-        Barco barco2 = new Portaviones(35, new Motor(), 250, 75, 46, 60000, 7);
+        Barco barco1 = new Yate(true, 4, 6, new Motor(345), 20.0, 12.5, 23, 71);
+        Barco barco2 = new Portaviones(35, new Motor(456), 250, 75, 46, 60000, 7);
         
         System.out.println(barco1 instanceof Portaviones);
        /* // Prueba to código aquí
